2591680f2383f3d2c4acb323eb8dc4c363584708
Priyanka Thakur <priyankathakur221206@gmail.com>
2022-02-13 23:42:32 +0530

    created form frontend

--- main.go
+++ main.go
@@ -6,9 +6,21 @@ import (
 	"net/http"
 )
 
+func formhandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Error in parsing form err: %v", err)
+		//fmt.Fprint(w, "hi")
+		return
+	}
+
+	fmt.Fprintf(w, "GET request successfully executed")
+
+}
+
 func main() {
 	fileserve := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileserve)
+	http.HandleFunc("/form", formhandler)
 
 	fmt.Print("server started at 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
